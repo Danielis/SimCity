fbb6c4eb0e13d95d540f90882894464ec4a1fbd4
Norman Chootong <normanchootong@yahoo.com>
2013-11-17 17:47:28 -0800

    Deleted bank
    Deleted all references of bank packages and the bank instantiation in
    citygui

--- src/bank/interfaces/Cashier.java
+++
@@ -1,7 +0,0 @@
-package bank.interfaces;
-
-public interface Cashier 
-{
-	public abstract void msgHereIsACheck(Teller newW, Customer newC, String newChoice);
-	public abstract void msgHereIsMyPayment(Customer newC, float payment);
-}
