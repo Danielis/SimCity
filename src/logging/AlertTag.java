5f0183fe1bb4e762a5194c06a09f43ca03ae1209
Chris Daniels <chdaniel@usc.edu>
2013-11-24 15:28:44 -0800

    commit to pull code.  new files for message tracing.

---
+++ src/logging/AlertTag.java
@@ -0,0 +1,11 @@
+package logging;
+
+public enum AlertTag {
+	 PERSON,
+     BANK_TELLER,
+     BANK_CUSTOMER,
+     BUS_STOP,
+     RESTAURANT,                //For the demo code where you make a new restaurant
+     BANK,                        //For the demo code where you make a new bank
+     GENERAL_CITY
+}
