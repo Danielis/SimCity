174c8dd63bf5a29172ab0d9ea1c330848eb2ed09
Chris Daniels <chdaniel@usc.edu>
2013-11-25 21:46:22 -0800

    about to start adding trace panel outputs

--- src/logging/TrackerGui.java
+++ src/logging/TrackerGui.java
@@ -3,24 +3,29 @@ package logging;
 import java.awt.Dimension;
 import java.util.Date;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class TrackerGui {
 	
 	JFrame trackerFrame;
+	TracePanel tracker;
 
 	public TrackerGui() {
+		//creates window
 		trackerFrame = new JFrame("Tracker");
 	    trackerFrame.setSize(new Dimension(500,100));
 	    trackerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    trackerFrame.setResizable(false);
 	    trackerFrame.setVisible(true);
-	    
-	    TracePanel tracker = new TracePanel();
+
+	    //creates tracker panel.  
+	    tracker = new TracePanel();
         trackerFrame.getContentPane().add(tracker);
         tracker.setVisible(true);
         tracker.showAlertsForAllLevels();
         tracker.showAlertsForAllTags();
-        tracker.alertOccurred(new Alert(AlertLevel.MESSAGE, AlertTag.GENERAL_CITY, "chris", "test message", new Date()));
+        //test message
+        tracker.alertOccurred(new Alert(AlertLevel.ERROR, AlertTag.GENERAL_CITY, "chris", "test message", new Date()));
 	}
 }
