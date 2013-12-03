293a57325001859f9d38b120c1b89f159fecca3d
Chris Daniels <chdaniel@usc.edu>
2013-12-03 00:31:51 -0800

    added the role files

---
+++ src/housing/HousingWorkerRole.java
@@ -0,0 +1,40 @@
+package housing;
+
+import housing.LandlordAgent.HousingComplex;
+import housing.guis.HousingWorkerGui;
+import housing.interfaces.HousingWorker;
+import roles.Role;
+
+public class HousingWorkerRole extends Role implements HousingWorker{
+
+	@Override
+	public void HereIsMoney(HousingComplex complex, double bill) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void GoRepair(HousingComplex complex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setGui(HousingWorkerGui g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLandlord(LandlordAgent landlord) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgLeaveWork() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/housing/LandlordRole.java
@@ -0,0 +1,27 @@
+package housing;
+
+import housing.interfaces.HousingCustomer;
+import housing.interfaces.Landlord;
+import roles.Role;
+
+public class LandlordRole extends Role implements Landlord{
+
+	@Override
+	public void HereIsRent(HousingCustomer housingCustomer, double balance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void MyHouseNeedsRepairs(HousingCustomer housingCustomerAgent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgLeaveWork() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
